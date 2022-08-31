7e0f0ac17f7236ed43e4e0c7a3f93a53e7a68f1f
Victor <victorluz.lima@gmail.com>
2022-08-30 22:59:18 -0300

    correção de bugs

--- api/app/Controllers/Http/TelemetriasController.ts
+++ api/app/Controllers/Http/TelemetriasController.ts
@@ -6,7 +6,8 @@ export default class TelemetriasController {
 
     public async list({response,request,auth}:HttpContextContract)
     {
-        const dados = request.body();
+        let dados = request.body();
+        dados.data_final+= " 23:59:59";
         const query = await Database.from('ml_int_telemetria_kontrow_trips')
                             .join('ml_int_telemetria_kontrow_assets', 'ml_int_telemetria_kontrow_trips.asset_id' , '=' ,'ml_int_telemetria_kontrow_assets.asset_id')
                             .join('ml_int_telemetria_kontrow_drivers','ml_int_telemetria_kontrow_drivers.driver_id','=','ml_int_telemetria_kontrow_trips.drive_id')
@@ -21,19 +22,10 @@ export default class TelemetriasController {
                                 ml_man_frota.media_consumo as media_consumo_veiculo,
                                 (ml_int_telemetria_kontrow_trips.total_mileage / ml_int_telemetria_kontrow_trips.fuel_used) as media_consumo_viagem
                             `)).where(Database.raw(`ml_int_telemetria_kontrow_trips.fuel_used != 0 `))
-                            .where('ml_int_telemetria_kontrow_trips.date','>=',dados.data)
+                            .where('ml_int_telemetria_kontrow_trips.date','>=',dados.data_inicial)
+                            .where('ml_int_telemetria_kontrow_trips.date','<=',dados.data_final)
                             .where('ml_int_telemetria_kontrow_drivers.worker_id','=',`${auth.user?.id_funcionario}`)
                             .orderBy('media_consumo_viagem','desc');
         response.json(query);
-
-        /*
-                                    .select('ml_int_telemetria_kontrow_trips.id_viagem')
-                            .select('ml_int_telemetria_kontrow_drivers.worker_id')
-                            .select('ml_int_telemetria_kontrow_drivers.driver_name')
-                            .select('ml_int_telemetria_kontrow_trips.fuel_used')
-                            .select('ml_int_telemetria_kontrow_assets.description')
-                            .select('ml_man_frota.placa')
-                            .select('ml_man_frota.media_consumo')
-        */
     }
 }
