51f91e21229468df889556dd7f869b10262cbfa6
Victor Lima <victorluz.lima@gmail.com>
2022-09-07 14:03:23 -0300

    ajuste no endpoint de telemetria

--- api/app/Controllers/Http/TelemetriasController.ts
+++ api/app/Controllers/Http/TelemetriasController.ts
@@ -36,34 +36,60 @@ export default class TelemetriasController {
         response.json(query);
     }
 
+
+
+
+
+
     public async list_events({response,request,auth}:HttpContextContract)
     {
         await request.validate({schema: schema.create({ data : schema.date() })});
         let dados = request.body();
         dados.data+= " 00:00:00";
-        const query = await Database.from('ml_int_telemetria_evento_grupo_associa')
+
+        let query = await Database.from('ml_int_telemetria_evento_grupo_associa')
                             .join('ml_int_telemetria_evento_grupo','ml_int_telemetria_evento_grupo_associa.id_telemetria_grupo','=','ml_int_telemetria_evento_grupo.id_grupo')
                             .join('ml_int_telemetria_kontrow_evento','ml_int_telemetria_evento_grupo_associa.id_telemetria_evento','=','ml_int_telemetria_kontrow_evento.id_evento')
                             .join('ml_int_telemetria_kontrow_trips','ml_int_telemetria_evento_grupo_associa.id_telemetria_trip','=','ml_int_telemetria_kontrow_trips.id_viagem')
                             .join('ml_int_telemetria_kontrow_drivers','ml_int_telemetria_kontrow_drivers.driver_id','=','ml_int_telemetria_kontrow_trips.drive_id')
-                            .join('ml_int_telemetria_kontrow_assets','ml_int_telemetria_kontrow_trips.asset_id','=','ml_int_telemetria_kontrow_assets.asset_id')
-                            .join('ml_man_frota','ml_int_telemetria_kontrow_assets.id_veiculo','=','ml_man_frota.id_veiculo')
                             .select(Database.raw(`
-                            ml_int_telemetria_evento_grupo_associa.id_empresa,
-                            ml_int_telemetria_evento_grupo_associa.id_empresa_grupo,
-                            ml_int_telemetria_evento_grupo_associa.dt_cadastro,
-                            ml_int_telemetria_evento_grupo_associa.dt_alteracao,
-                            ml_int_telemetria_evento_grupo_associa.id_status,
-                            ml_int_telemetria_kontrow_evento.evento,
-                            ml_int_telemetria_kontrow_evento.id_evento,
-                            ml_int_telemetria_evento_grupo.grupo,
-                            ml_int_telemetria_evento_grupo.id_grupo,
-                            ml_man_frota.placa,
-                            ml_man_frota.prefixo
+                                distinct ml_int_telemetria_evento_grupo.id_grupo,
+                                ml_int_telemetria_evento_grupo.grupo
                             `))
                             .where('ml_int_telemetria_kontrow_drivers.worker_id','=',`${auth.user?.id_funcionario}`)
                             .where('ml_int_telemetria_kontrow_trips.date','>=',`${dados.data}`);
+        
+
+        await Promise.all(
+            query.map( async element => {
+                element["events"] = [];
+                const event =  await this.get_events(auth.user?.id_funcionario,element.id_grupo,dados.data).then(function(data) {
+                    return data;
+                });
+                element.events = event;
+            })
+        );
 
         response.json(query);
+    }
+
+
+    public async get_events(user_id ,id_grupo , data){
+
+        let events = await Database.from('ml_int_telemetria_evento_grupo_associa')
+                    .join('ml_int_telemetria_evento_grupo','ml_int_telemetria_evento_grupo_associa.id_telemetria_grupo','=','ml_int_telemetria_evento_grupo.id_grupo')
+                    .join('ml_int_telemetria_kontrow_evento','ml_int_telemetria_evento_grupo_associa.id_telemetria_evento','=','ml_int_telemetria_kontrow_evento.id_evento')
+                    .join('ml_int_telemetria_kontrow_trips','ml_int_telemetria_evento_grupo_associa.id_telemetria_trip','=','ml_int_telemetria_kontrow_trips.id_viagem')
+                    .join('ml_int_telemetria_kontrow_drivers','ml_int_telemetria_kontrow_drivers.driver_id','=','ml_int_telemetria_kontrow_trips.drive_id')
+                    .select(Database.raw(`
+                        ml_int_telemetria_kontrow_evento.id_evento,
+                        ml_int_telemetria_kontrow_evento.evento
+                    `))
+        .where('ml_int_telemetria_kontrow_drivers.worker_id','=',`${user_id}`)
+        .where('ml_int_telemetria_evento_grupo.id_grupo','=',`${id_grupo}`)
+        .where('ml_int_telemetria_kontrow_trips.date','>=',`${data}`);
+
+        return events;
+
     }
 }
