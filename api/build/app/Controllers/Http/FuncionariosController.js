eda63a6dd5625f9ada1fa15285f8f963c32630cf
Victor Lima <victorluz.lima@gmail.com>
2023-05-08 20:52:49 -0300

    build

--- api/build/app/Controllers/Http/FuncionariosController.js
+++ api/build/app/Controllers/Http/FuncionariosController.js
@@ -232,6 +232,7 @@ class FuncionariosController {
                                     FROM  globus.vw_flp_fichaeventosrecibo hol
                                 WHERE
                                 hol.codintfunc = ${funcionario?.id_funcionario_erp} and to_char(competficha, 'YYYY-MM') = '${dados.data}'
+                                and hol.TIPOFOLHA = 1
                                 order by hol.tipoeven desc,hol.desceven
                                 `);
                 let empresa = await Empresa_1.default.findBy("id_empresa", auth.user?.id_empresa);
