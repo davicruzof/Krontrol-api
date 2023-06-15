97525da6b657b1174d92736b74881b1dea6bb502
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-06-15 14:39:32 -0300

    build version davi

--- api/build/env.js
+++ api/build/env.js
@@ -5,13 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Env_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Env"));
 exports.default = Env_1.default.rules({
-    HOST: Env_1.default.schema.string({ format: 'host' }),
+    HOST: Env_1.default.schema.string({ format: "host" }),
     PORT: Env_1.default.schema.number(),
     APP_KEY: Env_1.default.schema.string(),
     APP_NAME: Env_1.default.schema.string(),
-    DRIVE_DISK: Env_1.default.schema.enum(['local']),
-    NODE_ENV: Env_1.default.schema.enum(['development', 'production', 'test']),
-    PG_HOST: Env_1.default.schema.string({ format: 'host' }),
+    DRIVE_DISK: Env_1.default.schema.enum(["local"]),
+    NODE_ENV: Env_1.default.schema.enum(["development", "production", "test"]),
+    PG_HOST: Env_1.default.schema.string({ format: "host" }),
     PG_PORT: Env_1.default.schema.number(),
     PG_USER: Env_1.default.schema.string(),
     PG_PASSWORD: Env_1.default.schema.string.optional(),
@@ -20,7 +20,7 @@ exports.default = Env_1.default.rules({
     AWS_SECRET_ACCESS_KEY: Env_1.default.schema.string(),
     AWS_S3_BUCKET: Env_1.default.schema.string(),
     AWS_REGION: Env_1.default.schema.string(),
-    ORACLE_HOST: Env_1.default.schema.string({ format: 'host' }),
+    ORACLE_HOST: Env_1.default.schema.string({ format: "host" }),
     ORACLE_PORT: Env_1.default.schema.number(),
     ORACLE_USER: Env_1.default.schema.string(),
     ORACLE_PASSWORD: Env_1.default.schema.string.optional(),
