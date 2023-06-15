6e0f0f35568fa55b51a7a799d60b7c1c4bde8ad0
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-06-15 14:33:54 -0300

    Update OthersController.ts

--- api/app/Controllers/Http/OthersController.ts
+++ api/app/Controllers/Http/OthersController.ts
@@ -11,10 +11,6 @@ const TOKEN =
 const api = axios.create({
   baseURL: BASE_URL_SP_TRANS,
   timeout: 10000,
-  headers: {
-    Cookie:
-      "apiCredentials=ADD73F58F18CDEBAEE5538FFF8B08683F5F486076647976F0D8980C8324F7E34C592388DDA1CB9D95738D310B8D0EF0FA39E43B0B7FE699039BB3246816C65875A9464345A0128604AAE4452B4EAA4476E34692C8BFA5C8FF791749EB580BF0C45BAE8394D1AB28AC5FD40E55F5143DD86D1E37EE31F2B8491C4DA615ADC6154E87E15ED74FE6722BC1F9C1A70C41695C9CA2D5F0EED67472AC5479EF5C5B7F61030FDAC546848B818B218B1C3B2697F82A17D992C371EB7C105390EB7AF26B063BE6E71",
-  },
 });
 
 export default class OthersController {
@@ -29,13 +25,21 @@ export default class OthersController {
     try {
       const id_empresa = await this.getEnterpriseId(auth);
 
-      await api.post(`/Login/Autenticar?token=${TOKEN}`);
+      const resultAuth = await api.post(`/Login/Autenticar?token=${TOKEN}`);
 
-      const dados = await api.get(
-        `/Posicao/Garagem?codigoEmpresa=${id_empresa}`
-      );
+      if (resultAuth.data) {
+        const dados = await api.get(
+          `/Posicao/Garagem?codigoEmpresa=${id_empresa}`,
+          {
+            headers: {
+              Cookie: resultAuth.headers["set-cookie"],
+            },
+          }
+        );
 
-      return dados.data;
+        return dados.data;
+      }
+      return [];
     } catch (error) {
       response.badRequest({ error: "Erro interno 2" });
     }
