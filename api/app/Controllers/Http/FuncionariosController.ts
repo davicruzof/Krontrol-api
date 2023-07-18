0fbb0a9158f72e74a41b46a4e3e289db0382349f
Victor Lima <victorluz.lima@gmail.com>
2023-07-18 12:53:37 -0300

    build

--- api/app/Controllers/Http/FuncionariosController.ts
+++ api/app/Controllers/Http/FuncionariosController.ts
@@ -668,7 +668,7 @@ export default class FuncionariosController {
                                     FROM VW_ML_PON_FICHAPONTO F
                                     WHERE ID_FUNCIONARIO_ERP = '23364'
                                     AND DATA_MOVIMENTO BETWEEN to_date('${periodoInicial}','DD-MM-YYYY') and to_date('${periodoFinal}','DD-MM-YYYY')
-                                    ORDER BY DATA_MOVIMENTO
+                                    ORDER BY BH_COMPETENCIA
                       `);
           let resumoFicha = [];
 
