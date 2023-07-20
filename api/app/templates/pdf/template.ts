26ee9943eed02c743ec7f630f9e1497ab826373c
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-07-20 00:59:08 -0300

    adjust font size

--- api/app/templates/pdf/template.ts
+++ api/app/templates/pdf/template.ts
@@ -399,7 +399,7 @@ export const fichaPonto = `
                 <td style= "width:9%;">{{this.NORMAL}}</td>
                 <td style= "width:9%;">{{this.EXTRA}}</td>
                 <td style= "width:9%;">{{this.OUTRA}}</td>
-                <td style= "width:15%; fontSize: 12px;">{{this.OCORRENCIA}}</td>
+                <td style= "width:15%; font-size: 12px;">{{this.OCORRENCIA}}</td>
             </tr>
         {{/each}}
     </table>
