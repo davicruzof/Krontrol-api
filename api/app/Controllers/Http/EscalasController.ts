5289dd25fe5d678219da95b6605d4580b19f264a
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-08-26 16:05:58 -0300

    adjust escala

--- api/app/Controllers/Http/EscalasController.ts
+++ api/app/Controllers/Http/EscalasController.ts
@@ -51,7 +51,6 @@ export default class EscalasController {
       "id_funcionario",
       auth.user?.id_funcionario
     );
-    console.log(funcionario);
     let query: string;
     let campos: string;
     let tipo: string;
@@ -75,7 +74,7 @@ export default class EscalasController {
                   left join globus.vw_funcionarios func on esc.cod_cobrador = func.CODINTFUNC
                   left  join globus.t_esc_localidade locm on esc.COD_PEG_MOT = locm.COD_LOCALIDADE
                   left  join globus.t_esc_localidade locc on esc.COD_PEG_COB = locc.COD_LOCALIDADE
-                  WHERE to_char(esc.dat_escala, 'YYYY-MM-DD') = '${data}' and :tipo = '23739'`;
+                  WHERE to_char(esc.dat_escala, 'YYYY-MM-DD') = '${data}' and :tipo = '${funcionario?.id_funcionario_erp}'`;
 
     let result1 = await Database.connection("oracle").rawQuery(
       campos + query.replace(":tipo", tipo)
--- api/build/app/Controllers/Http/EscalasController.js
+++ api/build/app/Controllers/Http/EscalasController.js
@@ -39,7 +39,6 @@ class EscalasController {
     async getList({ request, auth, response }) {
         let data = request.params().data;
         let funcionario = await Funcionario_1.default.findBy("id_funcionario", auth.user?.id_funcionario);
-        console.log(funcionario);
         let query;
         let campos;
         let tipo;
@@ -63,7 +62,7 @@ class EscalasController {
                   left join globus.vw_funcionarios func on esc.cod_cobrador = func.CODINTFUNC
                   left  join globus.t_esc_localidade locm on esc.COD_PEG_MOT = locm.COD_LOCALIDADE
                   left  join globus.t_esc_localidade locc on esc.COD_PEG_COB = locc.COD_LOCALIDADE
-                  WHERE to_char(esc.dat_escala, 'YYYY-MM-DD') = '${data}' and :tipo = '23739'`;
+                  WHERE to_char(esc.dat_escala, 'YYYY-MM-DD') = '${data}' and :tipo = '${funcionario?.id_funcionario_erp}'`;
         let result1 = await Database_1.default.connection("oracle").rawQuery(campos + query.replace(":tipo", tipo));
         campos = ` SELECT
       pre.prefixoveic AS prefixo,
