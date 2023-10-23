cb488e5b723ddc0a455bb53b8b5aedfdf89679dc
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2023-10-23 18:19:54 -0300

    adjust pdf

--- api/app/Controllers/Http/AuthController.ts
+++ api/app/Controllers/Http/AuthController.ts
@@ -103,6 +103,7 @@ export default class AuthController {
     const queryGetUser = `
       SELECT usu.id_usuario,usu.id_status,func.id_funcionario,
       func.id_grupo,func.id_empresa,func.nome,func.cpf,
+      func.id_funcionario_erp,
       func.celular,func.email,func.dt_nascimento
       FROM ml_fol_funcionario func
         INNER JOIN ml_usu_usuario usu ON (usu.id_funcionario = func.id_funcionario)
